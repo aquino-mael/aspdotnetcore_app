527ddebf10b468649b6183b9838dbf4b80dd8768
Ismael Aquino <ibranco@brasiliano.com.br>
2021-10-29 17:20:15 -0300

    create root widget

---
+++ lib/ui/widget/aspdotnetcore_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../screens/screens.dart';
+
+class AspDotNetCoreApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginScreen(),
+    );
+  }
+}
