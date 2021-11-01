8593a7fded89f5a8256d3866f435e074984d788a
Ismael Aquino <ibranco@brasiliano.com.br>
2021-11-01 15:32:11 -0300

    create authentication service implementation

---
+++ lib/data/service/authentication_service.dart
@@ -0,0 +1,29 @@
+import 'package:aspnetcore_app/data/models/models.dart';
+
+import '../../domain/dtos/login/login.dart';
+import '../../domain/entities/entities.dart';
+import '../../domain/interfaces/services/services.dart';
+import '../http/http.dart';
+
+class AuthenticationService implements IAuthenticationService {
+  HttpClient client;
+  String url;
+
+  AuthenticationService({
+    required this.client,
+    required this.url,
+  });
+
+  @override
+  Future<UserEntity> login(LoginDto loginDto) async {
+    try {
+      final response = await client.postAsync("$url/login", {
+        "email": loginDto.email,
+      });
+
+      return UserModel.fromJson(response).toEntity();
+    } catch (e) {
+      throw e;
+    }
+  }
+}
