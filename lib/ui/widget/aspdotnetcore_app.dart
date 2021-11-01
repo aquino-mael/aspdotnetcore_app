738e0c2d4f9f4d760aaeb4127a76b23b34c16130
Ismael Aquino <ibranco@brasiliano.com.br>
2021-11-01 14:52:31 -0300

    add initialization of login service

--- lib/ui/widget/aspdotnetcore_app.dart
+++ lib/ui/widget/aspdotnetcore_app.dart
@@ -1,12 +1,21 @@
+import '../../domain/interfaces/services/services.dart';
 import 'package:flutter/material.dart';
 
 import '../screens/screens.dart';
 
 class AspDotNetCoreApp extends StatelessWidget {
+  final IAuthenticationService authenticationService;
+
+  const AspDotNetCoreApp({
+    required this.authenticationService,
+  });
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginScreen(),
+      home: LoginScreen(
+        authenticationService: authenticationService,
+      ),
     );
   }
 }
