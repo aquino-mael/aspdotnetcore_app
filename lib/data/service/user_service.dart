72dd939e5f89d76e988aad0ab8a7c0a85b89b6e0
Ismael Aquino <ibranco@brasiliano.com.br>
2021-11-01 14:51:01 -0300

    change user service's implementation

--- lib/data/service/user_service.dart
+++ lib/data/service/user_service.dart
@@ -1,28 +1,66 @@
 import '../../domain/dtos/user/user.dart';
 import '../../domain/interfaces/services/services.dart';
+import '../http/http.dart';
 
 class UserService implements IUserService {
+  final HttpClient client;
+  final String url;
+
+  UserService(this.client, this.url);
+
   @override
-  Future<UserDtoCreateResult> createUser(UserDtoCreate userToCreate) {
-    // TODO: implement createUser
-    throw UnimplementedError();
+  Future<UserDtoCreateResult> createUser(UserDtoCreate userToCreate) async {
+    try {
+      final response = await client.postAsync(url, userToCreate.toMap());
+      return UserDtoCreateResult.fromJson(response);
+    } catch (e) {
+      throw e;
+    }
   }
 
   @override
-  Future<bool> deleteUser(String id) {
-    // TODO: implement deleteUser
-    throw UnimplementedError();
+  Future<bool> deleteUser(String id) async {
+    try {
+      final response = await client.deleteAsync(url, id);
+      return response;
+    } catch (e) {
+      throw e;
+    }
   }
 
   @override
-  Future<UserDto> login(String email) {
-    // TODO: implement login
-    throw UnimplementedError();
+  Future<UserDtoUpdateResult> updateUser(UserDtoUpdate userToUpdate) async {
+    try {
+      final response = await client.putAsync(url, userToUpdate.toMap());
+      return UserDtoUpdateResult.fromJson(response);
+    } catch (e) {
+      throw e;
+    }
   }
 
   @override
-  Future<UserDtoUpdateResult> updateUser(UserDtoUpdate userToUpdate) {
-    // TODO: implement updateUser
-    throw UnimplementedError();
+  Future<List<UserDto>> getAll() async {
+    try {
+      final response = await client.getAsync(url);
+
+      final users = <UserDto>[];
+
+      return users;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  @override
+  Future<UserDto> getUser(String id) async {
+    try {
+      final response = await client.getAsync(url, queryParameters: {"id": id});
+
+      final user = UserDto.fromJson(response);
+
+      return user;
+    } catch (e) {
+      throw e;
+    }
   }
 }
