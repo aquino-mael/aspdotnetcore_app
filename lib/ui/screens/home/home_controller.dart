348ad36be32e498c17290cc7f36d3fa7d4a3f60d
Ismael Aquino <ismaellAquino@hotmail.com>
2021-11-01 18:30:49 -0300

    show users list on home screen

--- lib/ui/screens/home/home_controller.dart
+++ lib/ui/screens/home/home_controller.dart
@@ -1,7 +1,23 @@
+import 'dart:async';
+
+import '../../../domain/dtos/user/user.dart';
 import '../../../domain/interfaces/services/services.dart';
 
 class HomeController {
   final IUserService userService;
 
   HomeController(this.userService);
+
+  StreamController<List<UserDto>> users = StreamController<List<UserDto>>();
+
+  Future<void> getAllUsers() async {
+    final allUsers = await userService.getAll();
+    users.add(allUsers);
+  }
+
+  Future<void> registerUser(UserDtoCreate user) async {
+    await userService.createUser(user);
+
+    getAllUsers();
+  }
 }
--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../domain/dtos/user/user.dart';
 import 'home_controller.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -17,10 +18,59 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  HomeController get _controller => widget.controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.getAllUsers();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.refresh
+            ),
+            onPressed: _controller.getAllUsers,
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.person_add_alt_1
+            ),
+            onPressed: () async {
+              _controller.registerUser(
+                UserDtoCreate(
+                  email: "ismael@hotmail.com",
+                  name: "Ismael Aquino Teste",
+                )
+              );
+            },
+          ),
+        ],
+      ),
+      body: StreamBuilder<List<UserDto>>(
+        stream: _controller.users.stream,
+        builder: (context, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          return ListView(
+            children: snapshot.data!.map(_buildUsersList).toList(),
+          );
+        }
+      ),
+    );
+  }
+
+  Widget _buildUsersList(UserDto user) {
+    return ListTile(
+      title: Text(user.name),
+      subtitle: Text(user.email),
     );
   }
 }
